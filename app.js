717c38c4058b9670c5d6de2ada65e60470583f56
Mashkhura Abdukhakimova <00012864@wiut.uz>
2021-03-17 14:12:52 +0500

    create noteList page

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get('/create-note', (req, res) => {
 	res.render('create')
 })
 
+app.get('/notes', (req, res)=> {
+	res.render('notes', { noteList: ['First note', 'Second note'] })
+})
+
+app.get('/notes/detail', (req, res)=> {
+	res.render('detail')
+})
+
 app.listen(8000, err  => {
 	if (err) throw err
 
